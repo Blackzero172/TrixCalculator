fba87fc23b152a9c756a68081f8b4a30037c530f
Vo1dling <attlanticazz@hotmail.com>
2022-06-12 14:51:49 +0300

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Button, Pressable } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [playerNames, setNames] = useState(["", "", "", ""]);
+	return (
+		<SafeAreaView style={styles.container}>
+			{playerNames.map((name, i) => (
+				<View style={styles.inputContainer}>
+					<Text>Player {i + 1}</Text>
+					<TextInput
+						style={styles.input}
+						placeholder={`Enter Name...`}
+						onChangeText={(text) => {
+							setNames([...playerNames.slice(0, i), text, ...playerNames.slice(i + 1)]);
+						}}
+						value={name}
+					/>
+				</View>
+			))}
+			<Pressable style={styles.button}>
+				<Text>Continue</Text>
+			</Pressable>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	input: {
+		borderWidth: 2,
+		padding: 10,
+		paddingRight: 60,
+		textAlign: "left",
+	},
+	inputContainer: {
+		margin: 20,
+	},
+	button: {
+		borderWidth: 1,
+		backgroundColor: "red",
+		padding: 10,
+	},
 });
